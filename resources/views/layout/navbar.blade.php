951f445a677fe6ea2e0238aa596e94a1f3fd97e9
najafi <2014najafi@gmail.com>
2020-10-20 17:03:20 +0330

    fixed bug auth in section blade

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -23,25 +23,29 @@
             <li>
                 <a href="{{url('/contact-us')}}">Contact</a>
             </li>
-            <li>
-                <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdown_panel" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        {{\Illuminate\Support\Facades\Auth::user()->name . ' ' . \Illuminate\Support\Facades\Auth::user()->family}}<i class="fas fa-chevron-down ml-2" style="font-size: 12px"></i>
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdown_panel">
-                        <a class="dropdown-item" href="{{url('/home')}}">dashboard</a><br/>
-                        <a class="dropdown-item" href="#">Xaricdəki ünvanlarım</a><br/>
-                        <a class="dropdown-item" href="#">Sifarişlərim</a><br/>
-                        <a class="dropdown-item" href="#">Bağlamalarım</a><br/>
-                        <a class="dropdown-item" href="#">AZN Balansım</a><br/>
-                        <a class="dropdown-item" href="#">TL Balansım</a><br/>
-                        <a class="dropdown-item" href="#">Kuryer</a><br/>
-                        <a class="dropdown-item" href="#">Sorğu</a><br/>
-                        <a class="dropdown-item" href="{{url('/setting')}}">setting</a><br/>
-                        <a class="dropdown-item" href="{{url('/logout')}}">logout</a><br/>
+            @auth('web')
+                <li>
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdown_panel"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            {{\Illuminate\Support\Facades\Auth::user()->name . ' ' . \Illuminate\Support\Facades\Auth::user()->family}}
+                            <i class="fas fa-chevron-down ml-2" style="font-size: 12px"></i>
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdown_panel">
+                            <a class="dropdown-item" href="{{url('/home')}}">dashboard</a><br/>
+                            <a class="dropdown-item" href="#">Xaricdəki ünvanlarım</a><br/>
+                            <a class="dropdown-item" href="#">Sifarişlərim</a><br/>
+                            <a class="dropdown-item" href="#">Bağlamalarım</a><br/>
+                            <a class="dropdown-item" href="#">AZN Balansım</a><br/>
+                            <a class="dropdown-item" href="#">TL Balansım</a><br/>
+                            <a class="dropdown-item" href="#">Kuryer</a><br/>
+                            <a class="dropdown-item" href="#">Sorğu</a><br/>
+                            <a class="dropdown-item" href="{{url('/setting')}}">setting</a><br/>
+                            <a class="dropdown-item" href="{{url('/logout')}}">logout</a><br/>
+                        </div>
                     </div>
-                </div>
-            </li>
+                </li>
+            @endauth
         </ul>
     </div>
 </div>
