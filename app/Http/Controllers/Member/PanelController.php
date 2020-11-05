e0b4b937acde4d69738b5b28145ee438e59aeacf
Amir Taghati <amirtaghatiorg@gmail.com>
2020-11-05 12:52:29 +0330

    add payment table to front panel

--- app/Http/Controllers/Member/PanelController.php
+++ app/Http/Controllers/Member/PanelController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers\Member;
 
 use App\Http\Controllers\Controller;
+use App\Payment;
+use Illuminate\Support\Facades\Auth;
 
 class PanelController extends Controller
 {
     public function index()
     {
-        return view('members.panel');
+        $wallet = Auth::user()->balance;
+        $payments = Payment::where('user_id' , Auth::user()->id)
+            ->orderBy('created_at','DESC')
+            ->latest()
+            ->paginate(10);
+
+        return view('members.panel' , compact('payments' ,'wallet'));
     }
 }
