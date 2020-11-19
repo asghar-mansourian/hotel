d9e47ba90d7faa6396bde8a87ee2cf70d76be526
najafi <2014najafi@gmail.com>
2020-11-19 17:15:41 +0330

    refactor BlogController.php api and panel.blade.php

--- app/Http/Controllers/Api/V1/BlogController.php
+++ app/Http/Controllers/Api/V1/BlogController.php
@@ -3,93 +3,24 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Blog;
-use App\Http\Controllers\Admin\traits\ValidatorRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\Blogs as BlogResource;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    use ValidatorRequest;
 
     public function index(Request $request)
     {
-        $blogs = Blog::whereStatus('1')->paginate($request->input('paginate') ?? 10);
-
-        return new BlogResource($blogs);
+        return BlogResource::collection(
+            Blog::whereStatus('1')
+                ->orderBy('updated_at', $request->input('order_by') ?? Blog::sortType)
+                ->paginate($request->input('per_page') ?? 10)
+        );
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param \App\Blog $blog
-     * @return \Illuminate\Http\Response
-     */
     public function show(Blog $blog)
     {
-
-
         return new BlogResource($blog);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param \App\Blog $blog
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Blog $blog)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param \App\Blog $blog
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Blog $blog)
-    {
-
-//        $BlogValidate = new BlogRequest();
-//        $validate = $this->validateRules($BlogValidate->rules(), $request);
-//
-//        if ($validate != null) {
-//            return response()->json($this->validateRules($BlogValidate->rules(), $request), 422);
-//        }
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param \App\Blog $blog
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Blog $blog)
-    {
-        //
     }
 }
