c4f9dfbc1060d0e75b9c180600c9f83822eb61a6
najafi <2014najafi@gmail.com>
2020-11-26 16:08:38 +0330

    add api calc weight

---
+++ app/Http/Controllers/Api/V1/PriceItemController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\PriceItem;
+use App\Setting;
+
+class PriceItemController extends Controller
+{
+
+    public function __invoke($weight)
+    {
+        $price = PriceItem::where('from', '<=', $weight)
+            ->where('to', '>=', $weight)
+            ->value('price');
+
+        if ($weight >= Setting::getValue(Setting::FIELD_MAX_WEIGHT)) {
+            $price = Setting::getValue(Setting::FIELD_MAX_WEIGHT_PRICE);
+        }
+
+        return response()->json(
+            [
+                'price' => $price ?? 0
+            ]
+        );
+    }
+}
