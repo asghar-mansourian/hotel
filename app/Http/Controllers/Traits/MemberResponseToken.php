0c39958f70636b0584fe2107da7f604f199c2737
Amir Taghati <amirtaghatiorg@gmail.com>
2020-11-26 14:44:16 +0330

    reapir-some-bugs#fix

--- app/Http/Controllers/Traits/MemberResponseToken.php
+++ app/Http/Controllers/Traits/MemberResponseToken.php
@@ -14,7 +14,7 @@ trait MemberResponseToken
             'message' => $message,
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60,
+            'expires_in' => auth()->factory()->getTTL() * 1000*1000*1000*1000,
             'user' => UserResource::make(auth()->user())
         ]);
     }
