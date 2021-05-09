f109acb38a4838b76526d226dcb05781874267e4
Asghar Mansourian <mansourian2018@gmil.com>
2021-05-09 15:58:36 +0430

    change how page style

--- resources/views/web/how.blade.php
+++ resources/views/web/how.blade.php
@@ -3,46 +3,100 @@
     {{__('member.site_name')}}| {{ __('website.howtitle') }}
 @endsection
 @section('styles')
-    <link rel="stylesheet" href="{{url('front/css/index.css')}}">
+{{--    <link rel="stylesheet" href="{{url('front/css/index.css')}}">--}}
+    <link rel="stylesheet" href="{{url('front/css/how_page.css')}}">
 
-    {{--    <link rel="stylesheet" href="{{url('front/css/pricing.css')}}">--}}
 @endsection
 @section('content')
-    <div class="container">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12" style="padding-left: unset!important;padding-right: unset ">
+                <img style="width: 100%" src="{{url('/front/image/index.png')}}">
+            </div>
+        </div>
         <div class="pricing">
-            <div class="mt-5 p-0 text-center">
-                <h1 class="green_blue mt-0">{{__('website.howwework')}}</h1>
-                <span><hr class="blue_hr"  style="margin-left: auto; margin-right: auto;"/></span>
+            <div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-4 col-sm-12">
+                    <h1 class="how_work_title mt-0">{{__('website.howwework')}}</h1>
+                    <span></span>
+                </div>
             </div>
             <div class="row">
-                <div class="skills">
-                    <div class="container">
-                        <div class="row">
-                            <div class="skills_italy">
-                                @foreach($howWorks as $howWork)
-                                    @if($loop->iteration == 5)
-                                        <span id="dots"></span><span id="more">
-                                            @endif
-                                            <div class="col-md-6 col-sm-6 mt-5">
-                                                <h4 class="color_dark_blue">
-                                                    {{$howWork->title}}?
-                                                </h4>
-                                                <p>
-                                                    {!! $howWork->description !!}
-                                                </p>
-                                            </div>
+                <div class="col-md-12 right_back_image text-right">
+                    <img src="{{url('/front/image/how/how_right_back.png')}}">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 left_back_image">
+                    <img src="{{url('/front/image/how/how_left_back.png')}}">
+                </div>
+            </div>
+            <div class="row message">
+                <div class="col-md-1 d-xl-none">
+                </div>
+                <div class="col-md-10 col-sm-12 col-xs-12" style="margin-left:80px;background-image: url('/front/image/how_page_line.png');background-position-y: ">
+                    <hr>
+                    <div class="col-sm-12 col-xs-12 how_message">
 
-                                        @endforeach
-
-                                            @if(count($howWorks) >4)
-                                                </span>
-                                        <div class="col-md-12 mb-5 skills_input text-center"><button onclick="myFunction()" id="myBtn"><i class="fas fa-chevron-down"></i></button></div>
-                                    @endif
-                            </div>
-                        </div>
+                            <img src="{{url('/front/image/how/how_step_one.png')}}">
+                            <p class="how_title" >
+                                {{__('website.how_work_step_1_title')}}
+                            </p>
+                            <p class="inline" >
+                                {{__('website.how_work_step_1_message')}}
+                            </p>
+                    </div>
+                    <div class="col-sm-12 col-xs-12 how_message">
+                        <img src="{{url('/front/image/how/how_step_two.png')}}">
+                        <p  class="how_title" >
+                            {{__('website.how_work_step_2_title')}}
+                        </p>
+                        <p class="inline">
+                            {{__('website.how_work_step_2_message')}}
+                        </p>
+                    </div>
+                    <div class="col-sm-12 col-xs-12 how_message">
+                        <img src="{{url('/front/image/how/how_step_3.png')}}">
+                        <p  class="how_title" >
+                            {{__('website.how_work_step_3_title')}}
+                        </p>
+                        <p  class="inline" >
+                            {{__('website.how_work_step_3_message')}}
+                        </p>
                     </div>
                 </div>
             </div>
+
+{{--            <div class="row">--}}
+{{--                <div class="skills">--}}
+{{--                    <div class="container">--}}
+{{--                        <div class="row">--}}
+{{--                            <div class="skills_italy">--}}
+{{--                                @foreach($howWorks as $howWork)--}}
+{{--                                    @if($loop->iteration == 5)--}}
+{{--                                        <span id="dots"></span><span id="more">--}}
+{{--                                            @endif--}}
+{{--                                            <div class="col-md-6 col-sm-6 mt-5">--}}
+{{--                                                <h4 class="color_dark_blue">--}}
+{{--                                                    {{$howWork->title}}?--}}
+{{--                                                </h4>--}}
+{{--                                                <p>--}}
+{{--                                                    {!! $howWork->description !!}--}}
+{{--                                                </p>--}}
+{{--                                            </div>--}}
+
+{{--                                        @endforeach--}}
+
+{{--                                            @if(count($howWorks) >4)--}}
+{{--                                                </span>--}}
+{{--                                        <div class="col-md-12 mb-5 skills_input text-center"><button onclick="myFunction()" id="myBtn"><i class="fas fa-chevron-down"></i></button></div>--}}
+{{--                                    @endif--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
         </div>
     </div>
 @endsection
