ad4079519c5259d8412b150c82efe3358669d074
Asghar Mansourian <mansourian2018@gmil.com>
2021-05-11 09:16:38 +0430

    fix header menu font size

--- resources/views/layout/headerStyle.blade.php
+++ resources/views/layout/headerStyle.blade.php
@@ -5,7 +5,7 @@
 <link rel="icon" href="{{url('front/image/favicon/Untitled-1-09.svg')}}" type="image/x-icon">
 <link rel="stylesheet" href="{{url('front/css/bootstrap.min.css')}}">
 <link rel="stylesheet" href="{{url('front/css/bootstrap-select.min.css')}}">
-<link rel="stylesheet" href="{{url('front/css/style.css?v=1.1.4')}}">
+<link rel="stylesheet" href="{{url('front/css/style.css?v=1.1.5')}}">
 {{--<link rel="stylesheet" href="{{url('front/css/faq.css')}}">--}}
 {{--<link rel="stylesheet" href="{{url('front/css/blog.css')}}">--}}
 {{--<link rel="stylesheet" href="{{url('front/css/contact.css')}}">--}}
