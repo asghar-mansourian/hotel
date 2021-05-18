96f48710ef80cae69d38fb7867553c69f8713e9c
Asghar Mansourian <mansourian2018@gmil.com>
2021-05-18 15:35:27 +0430

    change blog page style

--- resources/views/web/blog.blade.php
+++ resources/views/web/blog.blade.php
@@ -4,105 +4,33 @@
 @endsection
 
 @section('styles')
-    <link rel="stylesheet" href="{{url('front/css/blog.css?v=1.0.0')}}">
+{{--    <link rel="stylesheet" href="{{url('front/css/blog.css?v=1.0.0')}}">--}}
 @endsection
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 text-center">
-                <div class="black pt-5"><span class="yellow mr-3">{{__('website.blog')}}</span></div>
-                <div class="italic">{{__('website.subtitle2')}}<br/>{{__('website.subtitle1')}}</div>
-            </div>
-            <div class="col-md-12 slider_search mt-5">
-                <div class="search text-center">
-                    <form method="post" action="{{route('blog.search')}}">
-                        @csrf
-                        <input type="text" name="search" placeholder="{{__('website.search')}} ...">
-                        <button type="submit">{{__('website.search')}}</button>
-                    </form>
-                </div>
-            </div>
-            <div class="col-md-12 mt-50">
-                <div class="row">
-                    <div class="col-md-12 col-sm-12 mt-5 mb-5 padding_25">
-                        <div class="padding_15">
-                            @php
-                                $topBlog = $blogs->count() ? $blogs->first() : null
-                            @endphp
-                            @if($topBlog)
-                                <div class="row">
-                                    <div class="col-md-7 p-0">
-                                        <div class="blog_mage">
-                                            <div class="blog_img_he">
-                                                <a href="{{url('/blog/' . $topBlog->slug)}}">
-                                                    <img src="{{url('images/' . $topBlog->picture)}}">
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-5">
-                                        <div class="mob_p-4">
-                                            <h4><strong>{{$topBlog->title}}</strong></h4>
-                                            <div class="line_he mt-2 mb-2">
-                                                {{\App\lib\Helpers::getContent($topBlog->content)}}
-                                            </div>
-                                            <div class="blog_a mt-4 mb-3">
-                                                <a href="{{url('/blog/' . $topBlog->slug)}}">{{__('website.readmore')}}<i class="fas fa-chevron-right ml-3"></i></a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            @endif
+    <div class="tm-parallax" style="background-position: center 840px;">
+        <header class="tm-parallax-header">
+            <h2 class="">{{__('website.blog')}}</h2>
+        </header>
+    </div>
+    <!-- Blog -->
+    <div class="site_center blog_out">
+        <div class="container mt-4 mb-5">
+            <div class="row mt-3">
+                @foreach($blogs as $blog)
+                <div class="col-md-4 col-sm-6 mb-4">
+                    <div class="blog">
+                        <div class="blog_img">
+                            <a href="{{url('/blog/' . $blog->slug)}}"><img src="{{url('/images/'.$blog->picture)}}" class="w-100"></a>
                         </div>
+                        <h5 class="blog_text"><strong><a href="{{url('/blog/' . $blog->slug)}}" class="color_blue">{{$blog->title}}</a></strong></h5>
+                        <p style="padding: 0 15px">{{\App\lib\Helpers::getContent($blog->content)}}</p>
+                        <div class="read_more"><a href="{{url('/blog/' . $blog->slug)}}">{{__('website.readmore')}}</a></div>
+                        <div class="blog_date">{{date('d-M-Y', strtotime($blog->created_at))}}</div>
+                        <div style="clear: both;"></div>
                     </div>
-                    @if($blogs->count())
-                        <div class="col-md-12 col-sm-12 black black_margin">{{__('website.latestnews')}}</div>
-                    @endif
-                    @foreach($blogs as $blog)
-                    <div class="col-md-6 col-sm-6 height_380 mt-5 mb-5 p-0">
-                        <div class="col-md-6 col-sm-6">
-                            <div class="blog_mage">
-                                <div class="blog_img">
-                                    <a href="{{url('/blog/' . $blog->slug)}}">
-                                        <img src="{{url('images/' . $blog->picture)}}" class="w-100">
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-sm-6">
-                            <strong class="color_blue">{{$blog->title}}</strong>
-                            <p class="font_grey mb-3">{{$blog->author->name}} <i class="far fa-comment ml-3 mr-1"></i> 3</p>
-                            <div class="mt-2 mb-2">
-                                {{\App\lib\Helpers::getContent($blog->content)}}
-                            </div>
-                            <div class="blog_a mt-4 mb-3">
-                                <a href="{{url('/blog/' . $blog->slug)}}">{{__('website.readmore')}}<i class="fas fa-chevron-right ml-3"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    @endforeach
-
                 </div>
-                {{$blogs->links()}}
+                @endforeach
             </div>
-            {{--
-                            <div class="col-md-12 mt-5 mb-5">
-                                <div class="number_list">
-                                    <ul>
-                                        <li class="mr-4"><a href="#"><span class="fa-fw select-all fas mr-0"></span></a></li>
-                                        <li><a href="#" class="">1</a></li>
-                                        <li><a href="#" class="border_left active">2</a></li>
-                                        <li><a href="#" class="border_left">3</a></li>
-                                        <li class="mr-3 ml-3">
-                                            <i class="fas fa-circle" style="font-size: 4px; margin-right: 0"></i>
-                                            <i class="fas fa-circle" style="font-size: 4px; margin-right: 0"></i>
-                                            <i class="fas fa-circle" style="font-size: 4px; margin-right: 0"></i>
-                                        </li>
-                                        <li><a href="#" class="">7</a></li>
-                                        <li class="ml-4"><a href="#"><span class="fa-fw select-all fas mr-0"></span></a></li>
-                                    </ul>
-                                </div>
-                            </div>
-        </div>--}}
         </div>
+    </div>
 @endsection
--- resources/views/web/singel.blade.php
+++ resources/views/web/singel.blade.php
@@ -1,63 +1,47 @@
 @extends('layout.layout')
-
+@section('styles')
+    <style>
+        .read_more a{
+            font-size: 11px!important;
+        }
+    </style>
+    @endsection
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 text-left">
-                <div class="pt-5 black_blog"><span class="yellow mr-3"><a href="/blog" class="yellow mr-3">{{__('website.blog')}}</a></span> <i
-                        class="fas fa-angle-right"></i> {{$blog->title}}</div>
-            </div>
-            <div class="col-md-12 mt-50">
-                <div class="row">
-                    <div class="col-md-8 col-sm-8 mt-5 mb-5">
-                        <div class="col-md-12 col-sm-12 col-xs-12 p-0">
-                            <div class="blog_mage">
-                                <div class="blog_img_he">
-                                    <img src="{{url('images/' . $blog->picture)}}" class="w-100"></div>
-                            </div>
+    <div class="tm-parallax" style="background-position: center 840px;">
+        <header class="tm-parallax-header">
+            <h2 class="">{{$blog->title}}</h2>
+        </header>
+    </div>
+    <!-- Blog in-->
+    <div class="site_center blog_in">
+        <div class="container mt-4 mb-5 blog_out">
+            <div class="row mt-3">
+                <div class="col-md-9 col-sm-12 mb-4 p-0">
+                    <div class="col-md-12">
+                        <h4 class="color_blue mb-3">{{$blog->title}}</h4>
+                        <div class="blog_in_img">
+                            <img src="{{url('/images/'.$blog->picture)}}" class="w-100">
+                            <div class="blog_date pl-0 pt-2" style="float: unset;">{{$blog->created_at}}</div>
                         </div>
-                        <div class="col-md-12 col-sm-12 col-xs-12">
-                            <div class="blog_sosyal ">
-                                <div class="blog_share">{{__('website.share')}}</div>
-                                <ul class="p-0">
-                                    <li>
-                                        <a href="https://www.facebook.com/sharer/sharer.php?u={{url('/blog/' . $blog->slug)}}" target="_blank"><i class="fab fa-facebook-f"></i></a>
-                                    </li>
-                                    <li>
-                                        <a href="#"><i class="fab fa-instagram"></i></a>
-                                    </li>
-                                    <li>
-                                        <a href="https://www.linkedin.com/shareArticle?mini=true&url={{url('/blog/' . $blog->slug)}}&title={{$blog->title}}&summary=&source=" target="_blank"><i
-                                                class="fab fa-linkedin-in"></i></a>
-                                    </li>
-                                    <li>
-                                        <a href="https://api.whatsapp.com/send?text={{url('/blog/' . $blog->slug)}}" target="_blank"><i class="fab fa-whatsapp"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-md-12">
-                            <h4><strong>{{$blog->title}}</strong></h4>
-                            <p class="font_grey mb-3">{{$blog->author->name}} <span class="fa-fw select-all fas ml-5"></span> 3</p>
-                            <div class="line_he mt-2 mb-2">{!! $blog->content !!}</div>
+                        <div class="color_blue">
+                            {!! $blog->content !!}
                         </div>
                     </div>
-                    <div class="col-md-4 col-sm-4 mt-5 mb-5  blog_padding">
-                        <div class="border_sh">
-                            <h3 class="mt-0"><strong>{{__('website.latestnews')}}</strong></h3>
-                            @foreach($last_news as $news)
+                </div>
+                <div class="col-md-3 col-sm-4 mb-4 mob_dis_no">
+                    <h5 class="color_gold mb-4">{{__('website.latestnews')}}</h5>
+                    @foreach($last_news as $news)
 
-                                <div class="blog_list">
-                                    <a href="{{url('/blog/' . $news->slug)}}">
-                                        <div class="news">
-                                            <img src="{{url('images/' . $news->picture)}}" style="    width: 41px; height:  41px;"></div>
-                                        <p class="news_p">{{$news->title}}</p></a>
-                                    <div class="font_grey text-right">{{$news->updated_at}}<span class="fa-fw select-all fas ml-0 mr-3"></span></div>
-                                </div>
-                            @endforeach
+                    <div class="blog mb-4">
+                        <div class="blog_img">
+                            <a href="{{url('/blog/' . $news->slug)}}"><img src="{{url('/images/'.$news->picture)}}" class="w-100"></a>
                         </div>
+                        <h6 class="blog_text"><strong><a  href="{{url('/blog/' . $news->slug)}}" class="color_blue">{{$news->title}}</a></strong></h6>
+                        <div class="read_more"><a href="{{url('/blog/' . $news->slug)}}">{{__('website.readmore')}}</a></div>
+                        <div class="blog_date">{{date('d-M-Y', strtotime($news->updated_at))}}</div>
+                        <div style="clear: both;"></div>
                     </div>
-
+                    @endforeach
                 </div>
             </div>
         </div>
