d5f316a907007943a5a6b65b4e6b89c5c8b1758a
Asghar Mansourian <mansourian2018@gmil.com>
2021-07-01 17:28:07 +0430

    change mobile icon

--- resources/views/web/contact.blade.php
+++ resources/views/web/contact.blade.php
@@ -31,13 +31,13 @@
                             </li>
                             <li>
                                 <div class="con_icon">
-                                    <span class="con_fac"><i class="fas fa-phone-volume"></i></span>
+                                    <span class="con_fac"><i class="fas fa-headset"></i></span>
                                 </div>
                                 <div class="con_desc"><p> {{\App\Setting::getValue(\App\Setting::FIELD_MOBILE)}}</p></div>
                             </li>
                             <li>
                                 <div class="con_icon">
-                                    <span class="con_fac"><i class="fas fa-phone-volume"></i></span>
+                                    <span class="con_fac"><i class="fas fa-headset"></i></span>
                                 </div>
                                 <div class="con_desc"><p> {{__('website.mobile_two')}}</p></div>
                             </li>
